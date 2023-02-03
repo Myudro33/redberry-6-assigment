2b6e0a5d532fe413689945ff1c07b7842e76578f
myudro33 <qanashvili.myudro@gmail.com>
2023-02-04 00:39:41 +0400

    personal page right side marckup

--- src/Pages/Personal/Personal.jsx
+++ src/Pages/Personal/Personal.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import 
- * as styled
- from "./PersonalStyled";
+import * as styled from "./PersonalStyled";
 import Navbar from "../../Components/Navbar";
 import { Button } from "../../Components/GlobalStyledComponents";
+import { FiAtSign } from "react-icons/fi";
+import { BsFillTelephoneFill } from "react-icons/bs";
 const Personal = () => {
   return (
     <styled.Container>
@@ -14,36 +14,81 @@ const Personal = () => {
             <styled.Label>
               სახელი
               <styled.Input type={"text"} placeholder="ანზორ" width={"371px"} />
-              <styled.Requirements>მინიმუმ 2 ასო, ქართული ასოები</styled.Requirements>
+              <styled.Requirements>
+                მინიმუმ 2 ასო, ქართული ასოები
+              </styled.Requirements>
             </styled.Label>
             <styled.Label>
               გვარი
-              <styled.Input type={"text"} placeholder="მუმლაძე" width={"371px"} />
-              <styled.Requirements>მინიმუმ 2 ასო, ქართული ასოები</styled.Requirements>
+              <styled.Input
+                type={"text"}
+                placeholder="მუმლაძე"
+                width={"371px"}
+              />
+              <styled.Requirements>
+                მინიმუმ 2 ასო, ქართული ასოები
+              </styled.Requirements>
             </styled.Label>
           </styled.NameContainer>
           <styled.FileUploadContainer>
             <styled.FileTitle>პირადი ფოტოს ატვირთვა</styled.FileTitle>
-            <label id="label" for="upload-photo">ატვირთვა</label>
+            <label id="label" for="upload-photo">
+              ატვირთვა
+            </label>
             <input type="file" name="photo" id="upload-photo" />
           </styled.FileUploadContainer>
-          <styled.Label style={{marginTop:'50px'}} >
+          <styled.Label style={{ marginTop: "50px" }}>
             ჩემს შესახებ (არასავალდებულო)
-            <styled.Textarea placeholder="ზოგადი ინფო შენს შესახებ"  />
+            <styled.Textarea placeholder="ზოგადი ინფო შენს შესახებ" />
           </styled.Label>
-          <styled.Label style={{marginTop:'50px'}} >
+          <styled.Label style={{ marginTop: "50px" }}>
             ელ.ფოსტა
-            <styled.Input placeholder="anzori666@redberry.ge" width={'100%'} type='email' />
-            <styled.Requirements>უნდა მთავრდებოდეს @redberry.ge-ით</styled.Requirements>
+            <styled.Input
+              placeholder="anzori666@redberry.ge"
+              width={"100%"}
+              type="email"
+            />
+            <styled.Requirements>
+              უნდა მთავრდებოდეს @redberry.ge-ით
+            </styled.Requirements>
           </styled.Label>
-          <styled.Label style={{marginTop:'50px'}} >
+          <styled.Label style={{ marginTop: "50px" }}>
             მობილურის ნომერი
             <styled.Input placeholder="+995 551 12 31 12" />
-            <styled.Requirements>უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს</styled.Requirements>
+            <styled.Requirements>
+              უნდა აკმაყოფილებდეს ქართული მობილურის ნომრის ფორმატს
+            </styled.Requirements>
           </styled.Label>
-          <Button type="submit" width='151px' >შემდეგი</Button>
+          <Button type="submit" width="151px">
+            შემდეგი
+          </Button>
         </styled.Form>
       </styled.LeftContainer>
+      <styled.RightContainer>
+        <styled.PersonalContainer>
+          <styled.PersonalInfoContainer>
+            <styled.NameTitle>ანზორ მუმლაძე</styled.NameTitle>
+            <styled.Contact>
+              <FiAtSign style={{ color: "#898989", marginRight: "5px" }} />{" "}
+              anzorr666@gmail.com
+            </styled.Contact>
+            <styled.Contact>
+              <BsFillTelephoneFill
+                style={{ color: "#898989", marginRight: "5px" }}
+              />{" "}
+              +995 597 63 45 16
+            </styled.Contact>
+            <styled.NameTitle style={{ fontSize: "18px", margin: "15px 0px" }}>
+              ჩემს შესახებ
+            </styled.NameTitle>
+            <styled.AboutMySelf>
+              ძალიან მიყვარს დიზაინის კეთება. დილით ადრე რომ ავდგები
+              გამამხნევებელი ვარჯიშების მაგიერ დიზაინს ვაკეთებ.
+            </styled.AboutMySelf>
+          </styled.PersonalInfoContainer>
+          <styled.PersonalImage/>
+        </styled.PersonalContainer>
+      </styled.RightContainer>
     </styled.Container>
   );
 };
--- src/Pages/Personal/PersonalStyled.js
+++ src/Pages/Personal/PersonalStyled.js
@@ -6,70 +6,92 @@ import styled from "styled-components";
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const RightContainer = styled.div`
-
+export const PersonalImage = styled.img`
+ width: 246px;
+ height: 246px;
+ border-radius: 50%;
+ border: 1px solid black;
+`
+export const AboutMySelf = styled.p`
+font-size: 16px;
+ color: #000000;
+ width: 430px;
 `
 
+export const Contact = styled.p`
+ color: #1A1A1A;
+ font-size: 18px;
+ display: flex;
+ align-items:center;
+ margin: 5px 0px;
+`
 
+export const NameTitle = styled.h1`
+  color: #f93b1d;
+  font-size: 34px;
+  font-weight: 700;
+  margin: 10px 0px;
+`;
 
+export const PersonalInfoContainer = styled.div`
+  width: 400px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
 
+export const PersonalContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  display: flex;
+  justify-content: space-between;
+`;
 
-
-
-
-
-
+export const RightContainer = styled.div`
+  width: 822px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 80px 50px;
+`;
 
 //  მარცხენა მხარე !!!
 
 export const Textarea = styled.textarea`
-width: 100%;
-height: 103px;
-margin: 5px 0px;
-border: 1px solid #BCBCBC;
-resize: none;
-padding: 10px;
-&::placeholder {
+  width: 100%;
+  height: 103px;
+  margin: 5px 0px;
+  border: 1px solid #bcbcbc;
+  resize: none;
+  padding: 10px;
+  &::placeholder {
     color: #00000060;
   }
-`
+`;
 
 export const FileTitle = styled.h1`
- color: #1A1A1A;
- font-size: 18px;
- font-weight: 500;
-`
+  color: #1a1a1a;
+  font-size: 18px;
+  font-weight: 500;
+`;
 export const FileUploadContainer = styled.div`
-width: 100%;
-height: 20px;
-margin-top: 30px;
-display: flex;
-align-items: center;
-`
+  width: 100%;
+  height: 20px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+`;
 
 export const Requirements = styled.p`
-color: #2E2E2E;
-font-weight: 300;
-font-size: 14px;
-`
+  color: #2e2e2e;
+  font-weight: 300;
+  font-size: 14px;
+`;
 
 export const Input = styled.input`
   height: 48px;
   width: ${(props) => props.width};
-  border: ${props=>props.border?'1px solid red':'1px solid #bcbcbc'};
+  border: ${(props) => (props.border ? "1px solid red" : "1px solid #bcbcbc")};
   border-radius: 4px;
   padding: 10px;
   margin: 5px 0px;
